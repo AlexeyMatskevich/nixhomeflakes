8e0b29c61195a76769f7e59df0ac34d0ea8a036c
Alexey <github_job@mackevich.addymail.com>
2024-06-04 18:38:14 +0500

    First home-manager commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Hoom Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; }; 
+    in {
+      homeConfigurations = {
+        amourlive = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
